c61fe9156f7049d69ddd1f581dc23b24802d6ff0
mainurrahat <mainurrahat51@gmail.com>
2025-08-11 00:31:58 +0600

    add factorial calc. in feature-branch

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,8 +1,20 @@
-// basic_program.cpp
+// Add factorial function and example usage
 #include <iostream>
 using namespace std;
 
-int main() {
+int factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
     cout << "Hello, World!" << endl;
+
+    int num = 5;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
+
     return 0;
 }
